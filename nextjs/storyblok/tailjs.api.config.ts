f4201ec7e7f3150ca3448e505259d75a534b7882
Niels Kühnel <niels.kuhnel@gmail.com>
2025-03-18 11:36:09 +0100

    Update tailjs.api.config.ts

--- nextjs/storyblok/tailjs.api.config.ts
+++ nextjs/storyblok/tailjs.api.config.ts
@@ -3,4 +3,8 @@ import { ConsoleLogger, createApi } from "@tailjs/next/server";
 export default createApi({
   debugScript: true, // Useful to see what is going on, once first installed.
   extensions: [new ConsoleLogger()], // Add extensions here to store data etc.
+
+  json: true, // Also useful for debugging
+
+  resourcesPath: "./tmp",
 });
